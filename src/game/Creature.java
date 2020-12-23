5d88d5e8b8d12948c536e1db369b1ed02ad354ab
Benjamin Castro <castro.bafc@outlook.com>
2020-12-23 11:42:55 -0500

    added comments to test branch

--- src/game/Creature.java
+++ src/game/Creature.java
@@ -3,6 +3,8 @@ package game;
 import java.util.ArrayList;
 import java.util.List;
 
+//added comments to test branch
+
 public class Creature extends Displayable {
     private int Hp;
     private int HpMoves;
