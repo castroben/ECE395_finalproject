60d3b0a0470165511d7a66824a94a8ba46bdf2d8
Benjamin Castro <castro.bafc@outlook.com>
2020-12-23 12:31:31 -0500

    added one last unnecessary comment

--- src/game/Creature.java
+++ src/game/Creature.java
@@ -3,6 +3,8 @@ package game;
 import java.util.ArrayList;
 import java.util.List;
 
+//one final unnecessary comment
+
 public class Creature extends Displayable {
     private int Hp;
     private int HpMoves;
